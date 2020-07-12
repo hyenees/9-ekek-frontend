380bbdfecc117ace2dfc2852fdeec265d8ae7550
ki226 <tkddn088@gmail.com>
2020-07-12 19:56:40 +0900

    홈퍼니싱 아이디어 구현

---
+++ src/Pages/Main/Furnishing.js
@@ -0,0 +1,129 @@
+import React from "react";
+import styled from "styled-components";
+import ImgContainer from "./ImgContainer";
+
+class Furnishing extends React.Component {
+  state = {
+    category: [
+      "침실",
+      "거실",
+      "주방",
+      "홈오피스",
+      "아웃도어",
+      "욕실",
+      "어린이 IKEA",
+      "다이닝",
+      "현관",
+    ],
+    colorHandle: "",
+    furnishingData: [],
+    buttonClick: 0,
+  };
+
+  buttonColorChange = (e) => {
+    this.setState({
+      colorHandle: e,
+    });
+  };
+
+  moreButtonHandler = () => {
+    this.setState({
+      buttonClick: this.state.buttonClick + 1,
+    });
+  };
+
+  render() {
+    const { category, colorHandle, buttonClick } = this.state;
+    return (
+      <Wrapper>
+        <Title>홈퍼니싱 아이디어 더 보기</Title>
+        {category.map((category, idx) => (
+          <button
+            className={`${
+              colorHandle === idx ? "category-btn-clicked" : "category-btn"
+            }`}
+            key={idx}
+            onClick={() => this.buttonColorChange(idx)}
+          >
+            {category}
+          </button>
+        ))}
+
+        <ImgContainer buttonClick={buttonClick} />
+        <MoreProduct>
+          <MoreProductButton>
+            <button onClick={this.moreButtonHandler}>12개 더 보기</button>
+          </MoreProductButton>
+        </MoreProduct>
+      </Wrapper>
+    );
+  }
+}
+
+export default Furnishing;
+
+const Wrapper = styled.div`
+  margin: 55px 0 0 200px;
+  position: relative;
+
+  .category-btn {
+    border: none;
+    outline: none;
+    margin: 15px 8px 10px;
+    border-radius: 40px;
+    height: 40px;
+    font-size: 12px;
+    padding: 0 15px;
+    font-weight: bold;
+    color: #111111;
+    background-color: #f5f5f5;
+    transition: 0.5s;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #dfdfdf;
+    }
+  }
+  .category-btn-clicked {
+    border: none;
+    outline: none;
+    margin: 15px 8px 10px;
+    border-radius: 40px;
+    height: 40px;
+    font-size: 12px;
+    padding: 0 15px;
+    font-weight: bold;
+    color: white;
+    background-color: black;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+`;
+
+const Title = styled.h3`
+  font-weight: 1000;
+  color: #111111;
+`;
+
+const MoreProduct = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding: 0 20px;
+  margin-top: 50px;
+`;
+
+const MoreProductButton = styled.div`
+  width: 115px;
+  height: 40px;
+  position: relative;
+  margin: 0 auto;
+  left: -70px;
+  button {
+    width: 100%;
+    height: 100%;
+    border-radius: 40px;
+    border: none;
+    font-weight: bold;
+  }
+`;
---
+++ src/Pages/Main/FurnishingImgForm.js
@@ -0,0 +1,113 @@
+import React from "react";
+import "./FurnishingImgForm.scss";
+import arrowIcon from "./arrowIcon.png";
+
+class FurnishingImgForm extends React.Component {
+  state = {
+    dotDisplay: "",
+    articleDisplay: "",
+  };
+
+  dotShow = (id) => {
+    this.setState({
+      dotDisplay: id,
+    });
+  };
+  dotHide = () => {
+    this.setState({
+      dotDisplay: "",
+    });
+  };
+
+  articleShow = (productId) => {
+    this.setState(
+      {
+        articleDisplay: productId,
+      },
+      () => console.log(this.state.articleDisplay)
+    );
+  };
+
+  articleHide = () => {
+    this.setState({
+      articleDisplay: "",
+    });
+  };
+
+  render() {
+    const { furnishingInfo } = this.props;
+    const { dotDisplay, articleDisplay } = this.state;
+
+    return (
+      <div className={`furnishing-img item${furnishingInfo.id}`}>
+        <img
+          src={furnishingInfo.background_img}
+          alt="furnishing"
+          onMouseOver={() => this.dotShow(furnishingInfo.id)}
+          onMouseOut={() => this.dotHide(furnishingInfo.id)}
+        ></img>
+        {furnishingInfo.product.map((products, idx) => (
+          <div
+            key={idx}
+            onMouseEnter={() => this.dotShow(furnishingInfo.id)}
+            onMouseLeave={() => this.dotHide(furnishingInfo.id)}
+          >
+            <button
+              className={`furnishing-btn ${
+                products.product_name.split(" ")[0]
+              }`}
+              onMouseOver={() => this.articleShow(products.id)}
+              onMouseLeave={() => this.articleHide()}
+              style={
+                ({ position: "absoulute" },
+                {
+                  top: `${products.product_position_top}`,
+                  left: `${products.product_position_left}`,
+                  opacity: `${dotDisplay === furnishingInfo.id ? "1" : "0"}`,
+                })
+              }
+            >
+              <article
+                className="furnishing-product"
+                style={{
+                  display: `${
+                    articleDisplay === products.id ? "flex" : "none"
+                  }`,
+                }}
+              >
+                <ul className="product-info">
+                  <li className="product-name">
+                    {products.product_name.split(" ")[0]}
+                  </li>
+                  <li className="product-name-korean">
+                    {products.product_name.split(" ")[1]}
+                  </li>
+                  <li className="product-category">
+                    {products.product_category}
+                  </li>
+                  <li className="product-price">{products.product_price}</li>
+                </ul>
+                <img
+                  className="go-to-product"
+                  src={arrowIcon}
+                  alt="arrow"
+                ></img>
+              </article>
+              <div className="article-shadow"></div>
+              <div
+                className="mini-dot"
+                style={{
+                  top: `${products.product_position_top}`,
+                  left: `${products.product_position_left}`,
+                  opacity: `${dotDisplay === furnishingInfo.id ? "1" : "0"}`,
+                }}
+              ></div>
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default FurnishingImgForm;
